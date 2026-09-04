27a5ed0cb5645ad7d10a0f159ae80c8cf9ed39a8
thallars <arszemlyanikin@gmail.com>
2026-09-04 14:14:54 +0300

    feat(examples/kuka_iiwa): add teleop pipeline

---
+++ examples/kuka_iiwa/kuka_leader_to_kuka_iiwa_teleop.py
@@ -0,0 +1,94 @@
+import time
+
+from lerobot.model.kinematics import RobotKinematics
+from lerobot.processor import RobotAction, RobotObservation, RobotProcessorPipeline
+from lerobot.processor.converters import (
+    robot_action_observation_to_transition,
+    transition_to_robot_action,
+)
+from lerobot.robots.kuka_iiwa import KukaIiwa, KukaIiwaConfig
+from lerobot.robots.kuka_iiwa.robot_kinematic_processor import (
+    KukaEEBoundsAndSafety,
+    LeaderJointDeltaToFollowerEE,
+)
+from lerobot.teleoperators.kuka_leader import KukaLeader, KukaLeaderConfig
+from lerobot.utils.robot_utils import precise_sleep
+from lerobot.utils.visualization_utils import init_rerun, log_rerun_data
+
+FPS = 30
+URDF_PATH = "/home/thallars/Documents/RoboticsLab/lerobot/examples/kuka_iiwa/iiwa2_gripper_fix.urdf"
+
+def main():
+    follower_config = KukaIiwaConfig(port="...", id="my_kuka_iiwa")
+    leader_config = KukaLeaderConfig(port="/dev/ttyACM0", id="my_kuka_clone")
+
+    follower = KukaIiwa(follower_config)
+    leader = KukaLeader(leader_config)
+
+    follower_kinematics_solver = RobotKinematics(
+        urdf_path=URDF_PATH,
+        target_frame_name="gripper_base_link",
+        joint_names=[
+            "joint_1",
+            "joint_2",
+            "joint_3",
+            "joint_4",
+            "joint_5",
+            "joint_6",
+            "joint_7",
+        ],
+    )
+
+    leader_to_follower_ee = RobotProcessorPipeline[tuple[RobotAction, RobotObservation], RobotAction](
+        steps=[
+            LeaderJointDeltaToFollowerEE(
+                kinematics=follower_kinematics_solver,
+                leader_motor_names=[
+                    "joint_1",
+                    "joint_2",
+                    "joint_3",
+                    "joint_4",
+                    "joint_5",
+                    "joint_6",
+                    "joint_7",
+                ],
+                euler_order="xyz",
+                use_latched_reference=True,
+            ),
+            # KukaEEBoundsAndSafety(
+            #     end_effector_bounds={"min": [-1.0, -1.0, 0.0], "max": [1.0, 1.0, 1.5]},
+            #     max_ee_step_m=0.05,
+            # ),
+        ],
+        to_transition=robot_action_observation_to_transition,
+        to_output=transition_to_robot_action,
+    )
+
+    follower.connect()
+    leader.connect()
+
+    #init_rerun(session_name="kuka_leader_to_kuka_iiwa_teleop")
+
+    print("Starting teleop loop...")
+    while True:
+        t0 = time.perf_counter()
+
+        # Get robot observation
+        follower_obs = follower.get_observation()
+
+        # Get teleop observation
+        leader_action = leader.get_action()
+
+        # teleop joints -> robot EE action
+        follower_action = leader_to_follower_ee((leader_action, follower_obs))
+
+        # Send action to robot
+        _ = follower.send_action(follower_action)
+
+        #log_rerun_data(observation=follower_obs, action=follower_action)
+
+        precise_sleep(max(1.0 / FPS - (time.perf_counter() - t0), 0.0))
+
+
+if __name__ == "__main__":
+    main()
